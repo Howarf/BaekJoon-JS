487fcd1d8084c24209aeaaf7dee53a291a8d1167
Howarf <dwarf0809@naver.com>
2022-04-03 16:53:23 +0900

    1000

---
+++ src/1000.js
@@ -0,0 +1,12 @@
+var data = [];
+var rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.on('line', (line) => {
+  data = line.split(' ').map(el => parseInt(el));
+});
+rl.on('close', () => {
+  console.log(data[0] + data[1]);
+  process.exit();
+});
