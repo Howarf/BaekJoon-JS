65d7f4ca534d2010c63bc1a5007ce4b23d5bdbb5
Howarf <dwarf0809@naver.com>
2022-04-03 23:21:44 +0900

    1008,10869

---
+++ src/10869.js
@@ -0,0 +1,16 @@
+var data = [];
+var rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.on('line', (line) => {
+    data = line.split(' ').map(el => parseInt(el));
+});
+rl.on('close', () => {
+    console.log(data[0] + data[1]);
+    console.log(data[0] - data[1]);
+    console.log(data[0] * data[1]);
+    console.log(parseInt(data[0] / data[1]));
+    console.log(data[0] % data[1]);
+    process.exit();
+});
