fc5abd39252b2fb515f7a90074cf252ef695585c
Howarf <dwarf0809@naver.com>
2022-04-20 17:06:11 +0900

    ✅4673

--- src/step5/4673.js
+++ src/step5/4673.js
@@ -1,6 +1,16 @@
-let numPiece;
-function SplitNum(params) {
-    for(let i=10;i<=100;i++){
-        numPiece = String(i).split("").map(el=>Number(el));
+let createNum=[],numPiece;
+let result="";
+for(let i=1;i<=10000;i++){
+    numPiece = String(i);
+    let num = 0;
+    for(let a=0;a<numPiece.length;a++){
+        num += Number(numPiece[a]);
+    }
+    createNum.push(i+num);
+}
+for(let j=1;j<=10000;j++){
+    if(createNum.indexOf(j) === -1){
+        result += j+"\n";
     }
 }
+console.log(result);
