02da2c7876fe9b494e90e2b00c8844b01d2270c2
Howarf <dwarf0809@naver.com>
2022-04-21 22:34:10 +0900

    11654

---
+++ src/step6/11654.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input;
+if(process.platform=="linux"){
+    input = fs.readFileSync("/dev/stdin").toString().trim();
+}
+else{
+    input = fs.readFileSync("../data.txt").toString().trim();
+}
+console.log(input.charCodeAt(0));
