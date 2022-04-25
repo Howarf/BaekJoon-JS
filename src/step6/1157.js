e0395a7e9c6becda8ce0b46e6d73192c6daed49e
Howarf <dwarf0809@naver.com>
2022-04-26 01:55:53 +0900

    ✅1157

--- src/step6/1157.js
+++ src/step6/1157.js
@@ -1,17 +1,21 @@
 const fs = require("fs");
-let input,data=[];
-let result="";
+let input,max,index,overlap = false;
+const result = new Array(26).fill(0);
 if(process.platform=="linux"){
-    input = fs.readFileSync("/dev/stdin").toString().trim();
+    input = fs.readFileSync("/dev/stdin").toString().trim().toLowerCase();
 }
 else{
-    input = fs.readFileSync("../data.txt").toString().trim();
+    input = fs.readFileSync("../data.txt").toString().trim().toLowerCase();
 }
-for(let i=0;i<input.length;i++){
-    for(let j=97;j<123;j++){
-        if(j===input[i].toLowerCase().charCodeAt()){
-            data.push(j);
-        }
+for (let i = 0; i < input.length; i++) {
+  result[input.charCodeAt(i) - 97] ++;
+}
+max = Math.max.apply(null,result);
+index = result.indexOf(max);
+for(let j = 0; j < 26; j++){
+    if(result[j] === max && index != j){
+        overlap = true;
+        break;
     }
 }
-console.log(data.in)
+console.log(overlap ? "?" : String.fromCharCode(index+65));
