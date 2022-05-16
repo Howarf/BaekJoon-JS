6ec61e7bf1e7d1192c5083478af1be0f7bf07d00
Howarf <dwarf0809@naver.com>
2022-05-16 23:39:53 +0900

    ✅ 11653

---
+++ src/step8/11653.js
@@ -0,0 +1,22 @@
+let input,sum = [];
+const fs = require("fs")
+if(process.platform === "linux"){
+    input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+}
+else{
+    input = fs.readFileSync("../data.txt").toString().trim().split("\r\n");
+}
+let i = 2
+let num = Number(input.shift(0));
+while(true){
+    if(num % i === 0){
+        num = num/i;
+        sum.push(i);
+        i = 1;
+    }
+    i++;
+    if(i > num){
+        break;
+    }
+}
+console.log(sum.join("\n"));
