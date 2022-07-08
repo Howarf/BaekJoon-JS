17dd539be08cc5a2eca2dee11517d3facc879d80
Howarf <dwarf0809@naver.com>
2022-07-08 15:15:29 +0900

    ✅ 1085

---
+++ src/step13/1085.js
@@ -0,0 +1,11 @@
+let input;
+const fs = require("fs")
+if(process.platform === "linux"){
+    input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+}
+else{
+    input = fs.readFileSync("../data.txt").toString().trim().split(" ");
+}
+const [x, y, w, h] = [input[0],input[1],input[2],input[3]];
+const data = [x, y, w - x, h - y];
+console.log(Math.min.apply(null,data));
