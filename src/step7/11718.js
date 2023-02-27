54b0f7d6d5b7bff2ff7f4dc427ac62d66f45e1e7
Howarf <dwarf0809@naver.com>
2023-02-27 15:21:49 +0900

    ✅ 11718

---
+++ src/step7/11718.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input;
+if(process.platform=="linux"){
+    input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+}
+else{
+    input = fs.readFileSync("../data.txt").toString().trim().split("\r\n");
+}
+console.log(input.join("\n"));
