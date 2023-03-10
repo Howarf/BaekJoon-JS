64c3a1716bdf0edd1ba987fe13f2892fd01c0f21
Howarf <dwarf0809@naver.com>
2023-03-11 02:39:45 +0900

    ✅ 27866

---
+++ src/step5/27866.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let input;
+if(process.platform=="linux"){
+    input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+}
+else{
+    input = fs.readFileSync("../data.txt").toString().trim().split("\r\n");
+}
+let text = input.shift();
+let num = input.shift();
+console.log(text[num-1]);
