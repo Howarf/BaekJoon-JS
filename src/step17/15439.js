483d7a51f5d412bc2f4d9e478058f477a3e0299a
Howarf <dwarf0809@naver.com>
2023-04-05 00:03:06 +0900

    ✅ 15439

---
+++ src/step17/15439.js
@@ -0,0 +1,10 @@
+let input;
+const fs = require("fs")
+if(process.platform === "linux"){
+    input = fs.readFileSync("/dev/stdin").toString().trim();
+}
+else{
+    input = fs.readFileSync("../data.txt").toString().trim();
+}
+const num = +input;
+console.log(num * (num-1));
